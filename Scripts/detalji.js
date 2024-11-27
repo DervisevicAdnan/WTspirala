7a8bcd8c835074341647c6a89bd5ea6ab3cdd9b8
Adnan Dervisevic <dervisevicado@gmail.com>
2024-11-27 11:50:41 +0100

    Ispravka greske na prvom zadatku, omoguceno vracanje u pocetno stanje

--- Scripts/detalji.js
+++ Scripts/detalji.js
@@ -1,3 +1,19 @@
-let carouselControls = postaviCarousel(document.getElementById("upiti"),document.getElementsByClassName("upit"));
+let sviElementi = [...document.getElementsByClassName("upit")];
+
+let carouselControls = postaviCarousel(document.getElementById("upiti"), document.getElementsByClassName("upit"));
 document.getElementById("prethodni").addEventListener("click", carouselControls.fnLijevo);
 document.getElementById("sljedeci").addEventListener("click", carouselControls.fnDesno);
+
+window.addEventListener("resize", function(){
+    if(this.window.outerWidth > 600) {
+        let doc = this.document.getElementById("upiti");
+        doc.innerHTML = "";
+        for(let i = 0; i < sviElementi.length; i++){
+            doc.innerHTML += sviElementi[i].outerHTML;
+        }
+    }else{
+        this.carouselControls = postaviCarousel(this.document.getElementById("upiti"), this.document.getElementsByClassName("upit"));
+        this.document.getElementById("prethodni").addEventListener("click", this.carouselControls.fnLijevo);
+        this.document.getElementById("sljedeci").addEventListener("click", this.carouselControls.fnDesno);
+    }
+})
